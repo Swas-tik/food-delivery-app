ac2293aef8bc11b5da604dcfcde173a2d4210e8c
Swas-tik <thawaliswastik@gmail.com>
2024-08-25 10:33:42 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>Choose form a diverde menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your carving and elevate your dining experience. One delicious meal at a time.</p>
+
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
